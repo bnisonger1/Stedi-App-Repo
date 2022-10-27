96a38b32f548e3f5413e60010fa2fd325dfeb89b
bnisonger1 <nis22002@byui.edu>
2022-10-27 13:42:20 -0600

    "added 2fa verify"

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState, } from 'react';
-import { StyleSheet, Text, View, Image, TouchableOpacity, AsyncStorage, TextInput, Button } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity, AsyncStorage, TextInput, Button, Alert } from 'react-native';
 import  Navigation from './components/Navigation';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import OnboardingScreen from './screens/OnboardingScreen';
@@ -16,6 +16,7 @@ const App = () =>{
   const [phoneNumber, setPhoneNumber] = React.useState("");
   const [isLoggedIn,setIsLoggedIn] = React.useState(false);
   const [homeTodayScore, setHomeTodayScore] = React.useState(0);
+  const [tempCode, setTempCode] = React.useState(null);
 
    if (isFirstLaunch == true){
 return(
@@ -53,6 +54,46 @@ return(
           
          }}
         />
+
+        <TextInput
+          value={tempCode}
+          onChangeText={setTempCode}
+          style={styles.input2}
+          placeholderTextColor='#4251f5'
+          placeholder='Enter Code'>
+        </TextInput>
+        <Button
+        title='Verify'
+        style={styles.button}
+        onPress={async()=>{
+          console.log('Button 2 was pressed')
+
+          const loginResponse=await fetch(
+            'https://dev.stedi.me/twofactorlogin',
+            {
+            
+            method:'POST',
+            headers:{
+              'content-type' : 'application/text'
+              },
+              body:JSON.stringify({
+                phoneNumber,
+                oneTimePassword:tempCode
+              })
+            }
+          )
+          console.log(loginResponse.status)
+
+          if (loginResponse.status == 200){
+            const sessionToken = await loginResponse.text();
+            console.log('Session Token', sessionToken)
+            setIsLoggedIn(true);
+          }
+          else{
+            Alert.alert('Warning', 'An invalid Code was entered.')
+          }
+         }}
+        />
       </View>
     )
   }
@@ -70,7 +111,14 @@ return(
     margin: 12,
     borderWidth: 1,
     padding: 10,
-    marginTop:100
+    marginTop:250
+  },
+  input2: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    marginTop:50
   },
   margin:{
     marginTop:100
